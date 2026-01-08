c2248a4ccac7e7acfa73c03c762f4cab20ba0567
mrm <mrm1985@mail.ru>
2026-01-08 11:44:34 +0300

    project update

--- bybit_scryner_tg/config.py
+++ bybit_scryner_tg/config.py
@@ -9,5 +9,5 @@ SECRET_KEY: str = os.getenv('API_SECRET')
 
 # API-ключи Bybit
 TELEGRAM_TOKEN: str = os.getenv('TG_TOKEN')
-# TELEGRAM_CHAT_ID удалён, так как теперь используется chat_id пользователя
+# chat_id пользователя
 OWNER_CHAT_ID: int = int(os.getenv('OWNER_CHAT_ID'))
